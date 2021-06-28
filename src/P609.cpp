039ba42d9a80fc35881e21adf08d527da3abb4c2
Chintsai Hwo <jcglqmoyx@gmail.com>
2021-06-28 20:21:52 +0800

    update

---
+++ src/P609.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <sstream>
+#include<unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string> &paths) {
+        unordered_map<string, vector<string>> map;
+        for (string &p : paths) {
+            stringstream ss(p);
+            string root;
+            ss >> root;
+
+            string file;
+            while (ss >> file) {
+                size_t l = file.find('(');
+                string content = file.substr(l + 1, file.size() - l);
+                map[content].push_back(root + "/" + file.substr(0, l));
+            }
+        }
+        vector<vector<string>> res;
+        for (auto[x, y] : map) if (y.size() > 1) res.push_back(y);
+        return res;
+    }
+};
