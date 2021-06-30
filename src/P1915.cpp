4c25d4de5e08da4760c491565aa0f39aecdb56ae
Chintsai Hwo <jcglqmoyx@gmail.com>
2021-07-01 00:09:10 +0800

    update

---
+++ src/P1915.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+    const int N = 100010;
+
+    long long wonderfulSubstrings(string word) {
+        int n = (int) word.size();
+        int s[N][10];
+        int cnt[1024] = {1};
+        memset(s, 0, sizeof s);
+        long long res = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                s[i][j] += s[i - 1][j];
+                if (word[i - 1] - 'a' == j) s[i][j]++;
+            }
+            int state = 0;
+            for (int j = 0; j < 10; j++) {
+                state = state * 2 + s[i][j] % 2;
+            }
+            res += cnt[state];
+            for (int j = 0; j < 10; j++) {
+                res += cnt[state ^ (1 << j)];
+            }
+            cnt[state]++;
+        }
+        return res;
+    }
+};
